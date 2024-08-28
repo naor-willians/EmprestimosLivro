546f83920ca6fbedce1c15a896305e7c62f00245
Naor Willians <naorwillians@hotmail.com>
2024-08-28 14:45:33 -0300

    initial commit

---
+++ EmprestimosLivro/Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using EmprestimosLivro.Data;
+using EmprestimosLivro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimosLivro.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmprestimoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+
+            return View(emprestimos);
+        }
+    }
+}
---
+++ EmprestimosLivro/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmprestimosLivro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimosLivro.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set;}
+    }
+}
