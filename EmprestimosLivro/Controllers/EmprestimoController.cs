db08e523e490af37b440a16aa45038bdf476b9c7
Naor Willians <naorwillians@hotmail.com>
2024-09-04 16:36:48 -0300

    crud completed

--- EmprestimosLivro/Controllers/EmprestimoController.cs
+++ EmprestimosLivro/Controllers/EmprestimoController.cs
@@ -18,5 +18,77 @@ namespace EmprestimosLivro.Controllers
 
             return View(emprestimos);
         }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Add(emprestimo);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id) 
+        { 
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null) { NotFound(); }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimoEditado) 
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Emprestimos.Update(emprestimoEditado);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimoEditado);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Remove(emprestimo);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
